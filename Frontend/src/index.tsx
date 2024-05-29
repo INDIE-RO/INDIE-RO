c8eba75caacfdba305dec4e03a2de9d2270be68b
HyeryongChoi <chex1004@gmail.com>
2024-05-30 00:33:58 +0900

    refactor: 리액트 쿼리 관련 로직 index파일로 이동

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { router } from '@/router/router';
 
@@ -9,8 +12,30 @@ if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000 * 60 * 5 /** @TODO 논의 후 값 변경 필요 */,
+      gcTime: 1000 * 60 * 5,
+      throwOnError: true,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: () => {
+      alert('[Error] Something went wrong!');
+    },
+  }),
+});
+
+/** @TODO ErrorBoundary 추가 필요 */
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Suspense>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
