163ff2fcc4c1ff7567fd7ff55294540b654e9b2b
HyeryongChoi <chex1004@gmail.com>
2024-05-29 17:26:18 +0900

    chore: 스토리북 절대경로 및 import react 생략 가능하도록 설정

--- Frontend/.storybook/main.ts
+++ Frontend/.storybook/main.ts
@@ -1,21 +1,43 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
+  },
+  /** 스토리북 파일에서 import react 생략 가능*/
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
+  /** 웹팩 세팅 커스터마이징 */
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          /** 절대경로 세팅 */
+          '@': path.resolve(__dirname, '..', 'src'),
+        },
+      },
+    };
   },
 };
 export default config;
