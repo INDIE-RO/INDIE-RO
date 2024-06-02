d21df9958e2e781c8e61a375187678e23a8c0e86
hae-on <solbi2004@naver.com>
2024-06-02 16:24:03 +0900

    feat: 글로벌 스타일 및 theme 설정

---
+++ Frontend/src/styles/theme.ts
@@ -0,0 +1,70 @@
+const colors = {
+  primary: '#E9FF75',
+  pink: '#FED6FF',
+  red: '#FF6B6B',
+
+  white: '#FFFFFF',
+  gray: '#F5F5F5',
+  gray1: '#F2F2F2',
+  gray2: '#DEDEDE',
+  gray3: '#A9A9A9',
+  gray4: '#5A5A5A',
+  gray5: '#4D4D4D',
+  gray6: '#44454C',
+  gray7: '#404040',
+  gray8: '#25242D',
+  black: '#000000',
+} as const;
+
+const textColors = {
+  default: colors.gray7,
+  white: colors.gray,
+  light: colors.gray3,
+  deep: colors.gray5,
+} as const;
+
+const dividerColors = {
+  bar: colors.gray4,
+  stroke: colors.gray2,
+} as const;
+
+const backgroundColors = {
+  deep: colors.gray8,
+  normal: colors.gray6,
+  light: colors.gray1,
+} as const;
+
+const tagColors = {
+  category: colors.pink,
+  region: colors.white,
+  openingStatus: colors.primary,
+  dDay: colors.red,
+};
+
+const fontSizes = {
+  xxs: '11px',
+  xs: '12px',
+  sm: '14px',
+  md: '16px',
+  lg: '18px',
+  xl: '24px',
+} as const;
+
+const fontWeights = {
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+} as const;
+
+const theme = {
+  colors,
+  textColors,
+  dividerColors,
+  backgroundColors,
+  tagColors,
+  fontSizes,
+  fontWeights,
+};
+
+export default theme;
