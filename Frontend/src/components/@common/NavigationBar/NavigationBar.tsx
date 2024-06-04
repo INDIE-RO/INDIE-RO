6018cba0dd2a2307c4d72be34c1b922a52ff4764
hae-on <solbi2004@naver.com>
2024-06-04 13:05:48 +0900

    feat: NavigationBar 구현

---
+++ Frontend/src/components/@common/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,70 @@
+import { NavLink } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+import { NAVIGATION_MENU } from '@/constants/path';
+import theme from '@/styles/theme';
+
+import SvgIcon from '../Svg/SvgIcon';
+
+function NavigationBar() {
+  return (
+    <Container>
+      <List>
+        {NAVIGATION_MENU.map(({ variant, name, path }) => (
+          <Item key={variant}>
+            <Link to={path}>
+              {({ isActive }) => (
+                <>
+                  <SvgIcon
+                    variant={variant}
+                    stroke={isActive ? theme.colors.primary : theme.colors.white}
+                  />
+                  <LinkName isActive={isActive}>{name}</LinkName>
+                </>
+              )}
+            </Link>
+          </Item>
+        ))}
+      </List>
+    </Container>
+  );
+}
+
+export default NavigationBar;
+
+const Container = styled.nav`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 73px;
+  max-width: 430px;
+  background: ${theme.backgroundColors.deep};
+`;
+
+const List = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+  border-top: 1px solid ${theme.dividerColors.bar};
+`;
+
+const Item = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+  height: 60px;
+`;
+
+const Link = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LinkName = styled.span<{ isActive: boolean }>`
+  font-size: ${theme.fontSizes.xxs};
+  color: ${({ isActive }) => (isActive ? theme.colors.primary : theme.colors.white)};
+`;
--- Frontend/src/components/@common/index.ts
+++ Frontend/src/components/@common/index.ts
@@ -1,2 +1,5 @@
+export { default as DesktopLayout } from './DesktopLayout/DesktopLayout';
+export { default as MobileLayout } from './MobileLayout/MobileLayout';
+export { default as NavigationBar } from './NavigationBar/NavigationBar';
 export { default as SvgIcon } from './Svg/SvgIcon';
 export { default as SvgSprite } from './Svg/SvgSprite';
