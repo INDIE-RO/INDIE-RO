5b94c8c25a9f9af4176644e11ba2841e33d47c06
HyeryongChoi <chex1004@gmail.com>
2024-06-06 22:16:26 +0900

    feat: Chip 컴포넌트에 tag 디자인 추가

--- Frontend/src/components/@common/Chip/Chip.tsx
+++ Frontend/src/components/@common/Chip/Chip.tsx
@@ -1,11 +1,12 @@
-import { CSSProperties, PropsWithChildren } from 'react';
+import { CSSProperties, Fragment, PropsWithChildren } from 'react';
 
 import { SerializedStyles, css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+import SvgIcon from '@/components/@common/Svg/SvgIcon';
 import theme from '@/styles/theme';
 
-type ChipVariant = 'standard' | 'outline' | 'rounded';
+type ChipVariant = 'standard' | 'outline' | 'rounded' | 'tag';
 type ChipSize = 'sm' | 'md' | 'lg';
 
 export interface ChipProps extends PropsWithChildren {
@@ -14,11 +15,29 @@ export interface ChipProps extends PropsWithChildren {
   color: CSSProperties['color'];
   backgroundColor: CSSProperties['backgroundColor'];
   disabled: boolean;
+  width: number;
+  height: number;
 }
 
 function Chip(chipProps: Partial<ChipProps>) {
   const { children, ...restProps } = chipProps;
-  return <Wrapper {...restProps}>{children}</Wrapper>;
+  return (
+    <Wrapper {...restProps}>
+      <TextBox>{children}</TextBox>
+      {restProps.variant === 'tag' && (
+        <Fragment>
+          {' '}
+          <SvgIcon
+            variant='close'
+            fill={restProps.color ? restProps.color : theme.colors.white}
+            stroke={'none'}
+            width={11}
+            height={11}
+          />
+        </Fragment>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Chip;
@@ -34,15 +53,18 @@ type ChipVariantStylesProps = Partial<Pick<ChipProps, 'color' | 'backgroundColor
 
 const chipSizeStyles: ChipSizeStyles = {
   sm: css`
-    width: 16.3rem;
-    font-size: ${theme.fontSizes.md};
+    height: 3.3rem;
+    border-radius: 6px;
+    font-size: ${theme.fontSizes.sm};
   `,
   md: css`
-    width: 27.7rem;
-    font-size: ${theme.fontSizes.lg};
+    height: 5rem;
+    border-radius: 8px;
+    font-size: ${theme.fontSizes.md};
   `,
   lg: css`
-    width: 33.5rem;
+    height: 5.4rem;
+    border-radius: 12px;
     font-size: ${theme.fontSizes.lg};
   `,
 };
@@ -50,8 +72,6 @@ const chipSizeStyles: ChipSizeStyles = {
 const chipVariantStyles: ChipVariantStyles = {
   standard: ({ color, backgroundColor, disabled }) =>
     css`
-      border-radius: 12px;
-
       background-color: ${disabled
         ? theme.colors.gray3
         : backgroundColor
@@ -65,7 +85,6 @@ const chipVariantStyles: ChipVariantStyles = {
     css`
       border: 2px solid
         ${disabled ? theme.colors.gray4 : backgroundColor ? backgroundColor : theme.colors.primary};
-      border-radius: 12px;
       background-color: transparent;
       color: ${disabled ? theme.colors.gray4 : color ? color : theme.colors.primary};
     `,
@@ -82,18 +101,49 @@ const chipVariantStyles: ChipVariantStyles = {
 
       color: ${disabled ? theme.colors.gray4 : color ? color : theme.textColors.default};
     `,
+
+  tag: ({ color, backgroundColor }) => css`
+    height: 2.4rem;
+
+    padding: 0.6rem 0.8rem;
+
+    border-radius: 16px;
+    background-color: ${backgroundColor ? backgroundColor : theme.backgroundColors.normal};
+
+    color: ${color ? color : theme.textColors.white};
+    font-weight: ${theme.fontWeights.medium};
+    font-size: ${theme.fontSizes.xxs};
+    line-height: 11px;
+  `,
 };
 
 const Wrapper = styled.div<Partial<ChipProps>>`
+  position: relative;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
-  height: 5.4rem;
+
+  width: fit-content;
+  max-width: 37.5rem;
+  padding: 1.2rem 1.6rem;
+
   font-weight: ${theme.fontWeights.bold};
   transition: background-color 0.3s ease, 0.3s;
 
   ${({ size }) => chipSizeStyles[size ?? 'md']};
   ${({ variant, color, backgroundColor, disabled }) =>
     chipVariantStyles[variant ?? 'standard']({ color, backgroundColor, disabled })};
+
+  ${({ width }) => width && `width: ${width}rem`};
+  ${({ height }) => height && `height: ${height}rem`};
+`;
+
+const TextBox = styled.p`
+  width: 100%;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: center;
 `;
