1c6d9b59b04c74eb284be50e8e71fce4f6ef4e1d
HASEUNGHEE <ariul123@gmail.com>
2024-06-11 20:25:02 +0900

    feat: 사용자 맞춤정보 결과 조회 기능 구현 (#77)

--- Backend/Indiero/src/main/java/com/indiero/controller/PolicyController.java
+++ Backend/Indiero/src/main/java/com/indiero/controller/PolicyController.java
@@ -1,11 +1,11 @@
 package com.indiero.controller;
 
 import com.indiero.dto.response.DetailPolicyResponse;
+import com.indiero.dto.response.UserPolicyResponse;
 import com.indiero.service.PolicyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/policies")
@@ -20,5 +20,18 @@ public class PolicyController {
     @GetMapping("/{id}")
     public DetailPolicyResponse findPolicyById(@PathVariable Long id) {
         return policyService.findPolicyById(id);
+    }
+
+    // 사용자 맞춤정보 조회
+    @GetMapping("/user")
+    public UserPolicyResponse getUserPolicy(
+            @RequestParam(required = false) Integer size,
+            @RequestParam(required = false) Long lastPolicyId,
+            @RequestParam List<Integer> categoryIds,
+            @RequestParam List<Integer> regionIds,
+            @RequestParam int ageId,
+            @RequestParam(required = false, defaultValue = "1") int sortBy
+    ) {
+        return policyService.getUserPolicy(size, lastPolicyId, categoryIds, regionIds, ageId, sortBy);
     }
 }
