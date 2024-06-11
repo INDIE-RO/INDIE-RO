87d4458e60bf4af8e07d38bf248fcfff16c46d11
hae-on <solbi2004@naver.com>
2024-06-12 00:03:24 +0900

    feat: survey page 기본 ui 구현

--- Frontend/src/pages/SurveyPage/SurveyPage.tsx
+++ Frontend/src/pages/SurveyPage/SurveyPage.tsx
@@ -1,5 +1,35 @@
+import styled from '@emotion/styled';
+
+import { ChipButton, NavigableHeader } from '@/components/@common';
+
+import SurveyCategoryPage from './SurveyCategoryPage';
+import SurveyRegionPage from './SurveyRegionPage';
+
 function SurveyPage() {
-  return <section>SurveyPage</section>;
+  return (
+    <>
+      <NavigableHeader />
+      <Container>
+        <SurveyRegionPage />
+        <ButtonWrapper>
+          <ChipButton size='lg' width='100%'>
+            다음
+          </ChipButton>
+        </ButtonWrapper>
+      </Container>
+    </>
+  );
 }
 
 export default SurveyPage;
+
+const Container = styled.section`
+  position: relative;
+  padding: 4.8rem 2rem;
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 16px;
+  width: calc(100% - 40px);
+`;
