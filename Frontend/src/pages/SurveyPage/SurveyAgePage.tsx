42134e6ecbf9b9b410b542bcabfff93a14ef2489
hae-on <solbi2004@naver.com>
2024-06-12 00:13:14 +0900

    feat: survey age page ui 구현

--- Frontend/src/pages/SurveyPage/SurveyAgePage.tsx
+++ Frontend/src/pages/SurveyPage/SurveyAgePage.tsx
@@ -1,5 +1,42 @@
+import styled from '@emotion/styled';
+
+import { ChipInput, ProgressBar } from '@/components/@common';
+import theme from '@/styles/theme';
+
+import { useSurveyAgeMetaQuery } from './Survey.query';
+
 function SurveyAgePage() {
-  return <div>SurveyAgePage</div>;
+  const { ageMeta } = useSurveyAgeMetaQuery();
+
+  return (
+    <div>
+      <div style={{ height: '20px' }} />
+      <ProgressBar step={3} />
+      <div style={{ height: '20px' }} />
+      <Title>만 나이를 알려주세요!</Title>
+      <div style={{ height: '14px' }} />
+      <p>연령대에 맞는 정보들을 추천해드려요</p>
+      <div style={{ height: '60px' }} />
+      <ChipInputWrapper>
+        {ageMeta.map(age => (
+          <ChipInput key={age.id} variant='rounded' size='lg' width='100%'>
+            {age.name}
+          </ChipInput>
+        ))}
+      </ChipInputWrapper>
+    </div>
+  );
 }
 
 export default SurveyAgePage;
+
+const Title = styled.h1`
+  font-size: ${theme.fontSizes.xl};
+  font-weight: ${theme.fontWeights.semiBold};
+`;
+
+const ChipInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
