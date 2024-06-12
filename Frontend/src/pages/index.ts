f6622fcd5d5593300eb0a1a817ac902666d7d5ad
hae-on <solbi2004@naver.com>
2024-06-12 19:30:42 +0900

    chore: context provider export

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -1,3 +1,5 @@
 export { default as IntroPage } from './IntroPage';
 export { default as Homepage } from './Homepage';
 export { default as SurveyPage } from './SurveyPage/SurveyPage';
+
+export { default as SurveyContextProvider } from './SurveyPage/Survey.Context';
