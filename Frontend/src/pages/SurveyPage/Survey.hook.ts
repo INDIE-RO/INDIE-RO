0ace792351724da5fc022df934c31ed0a47f9e2c
hae-on <solbi2004@naver.com>
2024-06-12 20:19:11 +0900

    chore: 대소문자 수정

--- Frontend/src/pages/SurveyPage/Survey.hook.ts
+++ Frontend/src/pages/SurveyPage/Survey.hook.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent } from 'react';
 
-import { useSurveyContext } from './Survey.Context';
+import { useSurveyContext } from './Survey.context';
 import { SurveyValue } from './Survey.type';
 
 const useSurvey = () => {
