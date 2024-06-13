c796a7c7914a951754afef0caa9f6f69e5eecd60
hae-on <solbi2004@naver.com>
2024-06-13 16:55:47 +0900

    refactor: 이전 설문으로 이동하는 props 추가

--- Frontend/src/components/@common/NavigableHeader/NavigableHeader.tsx
+++ Frontend/src/components/@common/NavigableHeader/NavigableHeader.tsx
@@ -6,12 +6,16 @@ import { SvgIcon } from '@/components/@common';
 import { useEasyNavigate } from '@/hooks/@common';
 import theme from '@/styles/theme';
 
-function NavigableHeader() {
+interface NavigableHeaderProps {
+  onPrev?: () => void;
+}
+
+function NavigableHeader({ onPrev }: NavigableHeaderProps) {
   const { goBack, goHome } = useEasyNavigate();
 
   return (
     <Container>
-      <Link to='#' onClick={goBack}>
+      <Link to='#' onClick={onPrev ? onPrev : goBack}>
         <SvgIcon height={18} variant='arrowLeft' stroke='none' fill={theme.colors.white} />
       </Link>
       <Link to='#' onClick={goHome}>
