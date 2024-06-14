51f6f4dddd69a270f315e320d0e025fc272f062f
hae-on <solbi2004@naver.com>
2024-06-15 00:40:15 +0900

    feat: 설문 조사 항목일 때, confirm 띄우도록 구현

--- Frontend/src/components/@common/NavigationBar/NavigationBar.tsx
+++ Frontend/src/components/@common/NavigationBar/NavigationBar.tsx
@@ -1,28 +1,59 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import styled from '@emotion/styled';
 
-import { SvgIcon } from '@/components/@common';
-import { NAVIGATION_MENU } from '@/constants/path';
+import { DialogConfirm, SvgIcon } from '@/components/@common';
+import { NAVIGATION_MENU, PATH } from '@/constants/path';
+import { useConfirm } from '@/hooks/@common';
 import theme from '@/styles/theme';
 
 function NavigationBar() {
+  const { confirm } = useConfirm();
+  const navigate = useNavigate();
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMoveSurvey = async () => {
+    const confirmed = await confirm({ message: '설문을 수정할까요?' });
+
+    if (confirmed) {
+      setIsActive(!isActive);
+      navigate(PATH.SURVEY);
+      return;
+    }
+  };
+
   return (
     <Container>
       <List>
         {NAVIGATION_MENU.map(({ variant, name, path }) => (
           <Item key={variant}>
-            <Link to={path}>
-              {({ isActive }) => (
-                <>
-                  <SvgIcon
-                    variant={variant}
-                    stroke={isActive ? theme.colors.primary : theme.colors.white}
-                  />
-                  <LinkName isActive={isActive}>{name}</LinkName>
-                </>
-              )}
-            </Link>
+            {variant === 'modify' ? (
+              <DialogConfirm onClick={handleMoveSurvey}>
+                <SurveyLinkWrapper>
+                  <>
+                    <SvgIcon
+                      variant={variant}
+                      stroke={isActive ? theme.colors.primary : theme.colors.white}
+                    />
+                    <LinkName isActive={isActive}>{name}</LinkName>
+                  </>
+                </SurveyLinkWrapper>
+              </DialogConfirm>
+            ) : (
+              <Link to={path}>
+                {({ isActive }) => (
+                  <>
+                    <SvgIcon
+                      variant={variant}
+                      stroke={isActive ? theme.colors.primary : theme.colors.white}
+                    />
+                    <LinkName isActive={isActive}>{name}</LinkName>
+                  </>
+                )}
+              </Link>
+            )}
           </Item>
         ))}
       </List>
@@ -59,6 +90,14 @@ const Item = styled.li`
 `;
 
 const Link = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SurveyLinkWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
