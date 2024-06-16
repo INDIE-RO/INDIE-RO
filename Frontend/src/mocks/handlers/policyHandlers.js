052d5f71148967f04f6ee6e7994ec9bae9898ee1
hae-on <solbi2004@naver.com>
2024-06-16 15:01:29 +0900

    feat: policyDetail msw 로직 작성

--- Frontend/src/mocks/handlers/policyHandlers.js
+++ Frontend/src/mocks/handlers/policyHandlers.js
@@ -3,6 +3,8 @@ import { HttpResponse, delay, http } from 'msw';
 import { API_PATH } from '@/constants/path';
 import policyFixture from '@/mocks/fixtures/policy';
 
+import policyDetail from '../data/policyDetail.json';
+
 const policyHandlers = [
   http.get('/api' + API_PATH.POLICY_LIST, async ({ request }) => {
     const url = new URL(request.url);
@@ -16,6 +18,14 @@ const policyHandlers = [
     if (regionIds) return HttpResponse.json(policyFixture.getPolicyListByRegion(), { status: 200 });
 
     return HttpResponse.json(policyFixture.getPolicyList(), { status: 200 });
+  }),
+
+  http.get('/api/policies/:id', async ({ request }) => {
+    // 나중에 id로 정책 상세 정보를 가져올 때를 대비해 작성
+    const url = new URL(request.url);
+    const policyId = url.pathname.split('/').pop();
+
+    return HttpResponse.json(policyDetail, { status: 200 });
   }),
 ];
 
