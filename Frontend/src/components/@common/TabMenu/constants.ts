5544f2ef13ad0bd32e66dad1397157a7f141bb47
hae-on <solbi2004@naver.com>
2024-06-16 15:50:10 +0900

    refactor: tab 관련 로직 수정

--- Frontend/src/components/@common/TabMenu/constants.ts
+++ Frontend/src/components/@common/TabMenu/constants.ts
@@ -1,7 +1,6 @@
 import { CATEGORY_TYPE } from '@/constants/common';
-import { CategoryVariant } from '@/types/common';
 
-import { DetailPolicyTabVariant, Tab } from './type';
+import { Tab, TabVariant } from './type';
 
 export const DETAIL_POLICY_TAB_TYPE = {
   DESCRIPTION: 'description',
@@ -9,14 +8,14 @@ export const DETAIL_POLICY_TAB_TYPE = {
   OTHER_INFO: 'otherInfo',
 } as const;
 
-export const TOTAL_POLICY_TAB_MENUS: Tab<CategoryVariant>[] = [
+export const TOTAL_POLICY_TAB_MENUS: Tab<TabVariant>[] = [
   { value: CATEGORY_TYPE.JOB, label: '일자리' },
   { value: CATEGORY_TYPE.HOUSING, label: '주거' },
   { value: CATEGORY_TYPE.EDUCATION, label: '교육' },
   { value: CATEGORY_TYPE.WELFARE, label: '복지/문화' },
 ];
 
-export const DETAIL_POLICY_TAB_MENUS: Tab<DetailPolicyTabVariant>[] = [
+export const DETAIL_POLICY_TAB_MENUS: Tab<TabVariant>[] = [
   { value: DETAIL_POLICY_TAB_TYPE.DESCRIPTION, label: '상세내용' },
   { value: DETAIL_POLICY_TAB_TYPE.QUALIFICATION, label: '신청자격' },
   { value: DETAIL_POLICY_TAB_TYPE.OTHER_INFO, label: '기타사항' },
--- Frontend/src/pages/PolicyListPage.tsx
+++ Frontend/src/pages/PolicyListPage.tsx
@@ -1,29 +1,25 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 
 import styled from '@emotion/styled';
 
 import { BasicLayout } from '@/components/@common';
 import TabMenu from '@/components/@common/TabMenu/TabMenu';
-import { Tab, TabVariant } from '@/components/@common/TabMenu/type';
+import { TOTAL_POLICY_TAB_MENUS } from '@/components/@common/TabMenu/constants';
+import { TabVariant } from '@/components/@common/TabMenu/type';
+import useTabMenu from '@/components/@common/TabMenu/useTabMenu';
 import { PolicyFilterBottomSheet, PolicyList } from '@/components/Policy';
 import { TAB_ID_BY_VARIANT } from '@/constants/common';
 import { PATH } from '@/constants/path';
 import { useEasyNavigate } from '@/hooks/@common';
 import theme from '@/styles/theme';
-import { CategoryVariant } from '@/types/common';
 import { generateQueryString } from '@/utils/route';
 
 function PolicyListPage() {
   const { navigate } = useEasyNavigate();
-  const tabMenus: Tab<CategoryVariant>[] = [
-    { value: 'job', label: '일자리' },
-    { value: 'housing', label: '주거' },
-    { value: 'education', label: '교육' },
-    { value: 'welfare', label: '복지/문화' },
-  ];
-  const [selectedTabMenu, setSelectedTabMenu] = useState<TabVariant>(tabMenus[0].value);
+  const { selectedTabMenu, handleTabMenuClick } = useTabMenu(TOTAL_POLICY_TAB_MENUS[0].value);
+
   const changeTab = (selectedMenu: TabVariant) => {
-    setSelectedTabMenu(selectedMenu);
+    handleTabMenuClick(selectedMenu);
     navigate(
       `${PATH.POLICY_LIST}${generateQueryString({ categoryId: TAB_ID_BY_VARIANT[selectedMenu] })}`,
     );
@@ -33,7 +29,7 @@ function PolicyListPage() {
     <BasicLayout>
       <Wrapper>
         <TabMenu
-          tabMenus={tabMenus}
+          tabMenus={TOTAL_POLICY_TAB_MENUS}
           selectedTabMenu={selectedTabMenu}
           handleTabMenuSelect={changeTab}
         />
