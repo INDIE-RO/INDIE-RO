b4547b4c20d6d1bf8518a722dffd35bc58814dc1
hae-on <solbi2004@naver.com>
2024-06-20 23:35:35 +0900

    chore: msw 개발모드 끄기

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -13,9 +13,9 @@ import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 const enableMocking = async () => {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return;
+  // }
 
   const { worker } = await import('./mocks/browser');
   return worker.start();
