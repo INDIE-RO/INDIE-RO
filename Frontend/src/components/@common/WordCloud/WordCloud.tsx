d0897decd26689f1c5f43a73910d183fd7fe4743
Hyeryong Choi <chex1004@gmail.com>
2024-06-20 18:35:58 +0900

    refactor: 워드클라우드 라이브러리 교체 (#134)
    * chore: 기존 워드클라우드 라이브러리 삭제
    
    * chore: adorable-word-cloud 설치
    
    * refactor: 워드클라우드 라이브러리 교체 및 적용

--- Frontend/src/components/@common/WordCloud/WordCloud.tsx
+++ Frontend/src/components/@common/WordCloud/WordCloud.tsx
@@ -1,4 +1,4 @@
-import ReactWordcloud, { Callbacks, Options } from '@cyberblast/react-wordcloud';
+import { AdorableWordCloud, CloudWord, Options } from 'adorable-word-cloud';
 
 import { usePolicySearch } from '@/pages/PolicySearchPage/PolicySearch.hook';
 
@@ -6,34 +6,20 @@ import { useWordCloudQuery } from './WordCloud.query';
 
 const options: Options = {
   colors: ['#B0E650', '#ff7f0e', '#4DD5CB', '#568CEC', '#CE7DFF', '#4FD87D'],
-  enableTooltip: false,
-  deterministic: false,
   fontFamily: 'JalnanGothic',
-  fontSizes: [14, 60],
-  fontStyle: 'normal',
-  fontWeight: 'normal',
-  padding: 4,
-  rotations: 0,
-  rotationAngles: [-90, 90],
-  scale: 'sqrt',
-  spiral: 'rectangular',
-  transitionDuration: 1000,
-  enableOptimizations: true,
-  svgAttributes: {},
-  textAttributes: {},
-  tooltipOptions: {},
-} as const;
+  fontSizeRange: [20, 60],
+  rotationDivision: 0,
+};
 
 function WordCloud() {
   const { words } = useWordCloudQuery();
   const { search } = usePolicySearch();
 
-  const callbacks: Callbacks = {
-    getWordTooltip: word => `The word "${word.text}" appears ${word.value} times.`,
-    onWordClick: word => search(word.text),
+  const onWordClick = (word: CloudWord) => {
+    search(word.text);
   };
 
-  return <ReactWordcloud words={words} options={options} callbacks={callbacks} />;
+  return <AdorableWordCloud words={words} options={options} callbacks={{ onWordClick }} />;
 }
 
 export default WordCloud;
