e9ff05dca17d85b427310afd902b2b29cbddab2e
HyeryongChoi <chex1004@gmail.com>
2024-06-26 23:04:52 +0900

    feat: 모집 중인 경우에만 d-day 조건부 렌더링 적용

--- Frontend/src/components/Policy/PolicyDetailOverview/PolicyDetailOverview.tsx
+++ Frontend/src/components/Policy/PolicyDetailOverview/PolicyDetailOverview.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 import styled from '@emotion/styled';
 
 import { Tag } from '@/components/@common';
@@ -15,16 +17,20 @@ function PolicyDetailOverview({ policyInfo }: PolicyDetailOverviewProps) {
   return (
     <div>
       <TagContainer>
-        {tags.map(tag => (
-          <Tag
-            key={tag.id}
-            size='sm'
-            color={tag.id !== 4 ? 'default' : 'white'}
-            backgroundColor={theme.tagColors[TAG_BY_ID[tag.id]]}
-          >
-            {tag.name}
-          </Tag>
-        ))}
+        {tags.map(tag =>
+          tag.type === '디데이' && Number(tag.name.slice(2)) < 0 ? (
+            <Fragment key={tag.id} />
+          ) : (
+            <Tag
+              key={tag.id}
+              size='sm'
+              color={tag.id !== 4 ? 'default' : 'white'}
+              backgroundColor={theme.tagColors[TAG_BY_ID[tag.id]]}
+            >
+              {tag.name}
+            </Tag>
+          ),
+        )}
       </TagContainer>
       <div style={{ height: '10px' }} />
       <Title>{title}</Title>
