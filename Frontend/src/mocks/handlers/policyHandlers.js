6c861a7bcfc487ecd531bd0b78fe6c382bd35649
HyeryongChoi <chex1004@gmail.com>
2024-06-29 00:01:09 +0900

    feat: 정책목록 정렬 및 필터링 로직 구현

--- Frontend/src/mocks/handlers/policyHandlers.js
+++ Frontend/src/mocks/handlers/policyHandlers.js
@@ -1,6 +1,9 @@
 import { HttpResponse, delay, http } from 'msw';
 
 import { API_PATH, BASE_URL } from '@/constants/path';
+import categoryMeta from '@/mocks/data/categories.json';
+import openingStatusMeta from '@/mocks/data/openingStatus.json';
+import regionMeta from '@/mocks/data/regions.json';
 import policyFixture from '@/mocks/fixtures/policy';
 
 import policyDetail from '../data/policyDetail.json';
@@ -26,33 +29,51 @@ const policyHandlers = [
   /* 맞춤 정책목록 조회 */
   http.get(BASE_URL + '/api' + API_PATH.CUSTOM_INFO, async ({ request }) => {
     const url = new URL(request.url);
-    const regionIds = url.searchParams.get('regionIds');
-    const categoryIds = url.searchParams.get('categoryIds');
-    const sortBy = url.searchParams.get('sortBy');
+    const sortBy = parseInt(url.searchParams.get('sortBy'));
+    const categoryIds = url.searchParams.get('categoryIds')?.split(',').map(parseInt);
+    const regionIds = url.searchParams.get('regionIds')?.split(',').map(parseInt);
 
-    await delay(200);
+    const { policies, ...restResponse } = policyFixture.getPolicyList();
 
-    if (categoryIds && categoryIds.includes('2'))
-      return HttpResponse.json(policyFixture.getPolicyListByCategory(), { status: 200 });
-    if (regionIds) return HttpResponse.json(policyFixture.getPolicyListByRegion(), { status: 200 });
+    // 필터링(정책분야, 모집현황, 지역)
+    const filteredPolicies = filterPolicies({
+      policies,
+      categoryIds,
+      regionIds,
+    });
 
-    return HttpResponse.json(policyFixture.getPolicyList(), { status: 200 });
+    // 정렬
+    const sortedPolicies = sortPolicies(filteredPolicies, sortBy);
+
+    await delay(2000);
+
+    return HttpResponse.json({ ...restResponse, policies: sortedPolicies }, { status: 200 });
   }),
 
   /* 정책목록 조회 */
   http.get(BASE_URL + '/api' + API_PATH.POLICY_LIST, async ({ request }) => {
     const url = new URL(request.url);
-    const regionIds = url.searchParams.get('regionIds');
-    const categoryId = url.searchParams.get('categoryId');
-    const sortBy = url.searchParams.get('sortBy');
+    const sortBy = parseInt(url.searchParams.get('sortBy'));
+    const categoryId = parseInt(url.searchParams.get('categoryId'));
+    const openingStatusId = parseInt(url.searchParams.get('openingStatusId'));
+    const regionIds = url.searchParams.get('regionIds')?.split(',').map(parseInt);
+
+    const { policies, ...restResponse } = policyFixture.getPolicyList();
+
+    // 필터링(정책분야, 모집현황, 지역)
+    const filteredPolicies = filterPolicies({
+      policies,
+      categoryId,
+      openingStatusId,
+      regionIds,
+    });
+
+    // 정렬
+    const sortedPolicies = sortPolicies(filteredPolicies, sortBy);
 
     await delay(2000);
 
-    if (categoryId === '2')
-      return HttpResponse.json(policyFixture.getPolicyListByCategory(), { status: 200 });
-    if (regionIds) return HttpResponse.json(policyFixture.getPolicyListByRegion(), { status: 200 });
-
-    return HttpResponse.json(policyFixture.getPolicyList(), { status: 200 });
+    return HttpResponse.json({ ...restResponse, policies: sortedPolicies }, { status: 200 });
   }),
 
   http.get(BASE_URL + '/api' + API_PATH.POLICY_DETAIL, async ({ request }) => {
@@ -65,3 +86,69 @@ const policyHandlers = [
 ];
 
 export default policyHandlers;
+
+// 정책 목록을 정렬하는 함수
+function sortPolicies(policies, sortBy) {
+  // 마감순 정렬 (조회순 정렬은 조회수 데이터 없어서 모킹 안함)
+  if (sortBy !== 1) return policies;
+
+  const statusOrder = ['모집중', '모집예정', '상시모집', '마감'];
+
+  return policies.sort((a, b) => {
+    const statusA = statusOrder.indexOf(a.tags.find(tag => tag.type === '모집현황').name);
+    const statusB = statusOrder.indexOf(b.tags.find(tag => tag.type === '모집현황').name);
+
+    // 모집 상태가 같을 때는 디데이로 정렬
+    if (statusA === statusB) {
+      const dDayA = a.tags.find(tag => tag.type === '디데이').name.replace('D-', '');
+      const dDayB = b.tags.find(tag => tag.type === '디데이').name.replace('D-', '');
+      return parseInt(dDayA) - parseInt(dDayB);
+    }
+
+    return statusA - statusB;
+  });
+}
+
+// 정책 목록을 필터링하는 함수
+function filterPolicies({ policies, categoryId, openingStatusId, regionIds, categoryIds }) {
+  const { openingStatuses } = openingStatusMeta;
+  const { categories } = categoryMeta;
+  const { regions } = regionMeta;
+
+  const openingStatusName = getNameById(openingStatusId, openingStatuses);
+  const categoryName = getNameById(categoryId, categories);
+  const categoryNames = getNamesByIds(categoryIds, categories);
+  const regionNames = getNamesByIds(regionIds, regions);
+
+  return policies.filter(policy => {
+    const matchesOpeningStatus = openingStatusId
+      ? policy.tags.some(tag => tag.type === '모집현황' && tag.name === openingStatusName)
+      : true;
+
+    // 정책 모두보기 필터링
+    const matchesCategory = categoryId
+      ? policy.tags.some(tag => tag.type === '분야' && tag.name === categoryName)
+      : true;
+
+    // 맞춤정책 필터링
+    const matchesCategories = categoryIds
+      ? policy.tags.some(tag => tag.type === '분야' && categoryNames.includes(tag.name))
+      : true;
+
+    const matchesRegions = regionIds
+      ? policy.tags.some(tag => tag.type === '지역' && regionNames.includes(tag.name))
+      : true;
+
+    return matchesOpeningStatus && matchesCategory && matchesCategories && matchesRegions;
+  });
+}
+
+function getNameById(id, metaList) {
+  const metaItem = metaList.find(meta => parseInt(meta.id) === parseInt(id));
+
+  return metaItem ? metaItem.name : null;
+}
+
+function getNamesByIds(ids, metaList) {
+  return Array.isArray(ids) && ids.map(id => getNameById(id, metaList));
+}
