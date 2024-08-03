081f3e021f266cdc2641715b5affd725b81fea6b
Hyeryong Choi <chex1004@gmail.com>
2024-08-04 00:23:28 +0900

    chore: publicPath 수정

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
-    publicPath: '/',
+    publicPath: '/INDIE-RO/',
   },
   plugins: [
     new HtmlWebpackPlugin({
