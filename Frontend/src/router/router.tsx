c6ea27efa667bf92aa4bd5bad9ba36687aab2c06
hae-on <solbi2004@naver.com>
2024-08-06 22:50:38 +0900

    refactor: 다이나믹 import를 통한 코드 스플리팅
    Co-authored-by: Hyeryong Choi <HyeryongChoi@users.noreply.github.com>

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -1,64 +1,76 @@
+import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
 import { PATH } from '@/constants/path';
-import {
-  CustomInfoPage,
-  Homepage,
-  IntroPage,
-  LoadingPage,
-  NotFoundPage,
-  PolicyDetailPage,
-  PolicyListPage,
-  PolicySearchPage,
-  SurveyPage,
-} from '@/pages';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <IntroPage />,
-        },
-        {
-          path: PATH.HOME,
-          element: <Homepage />,
-        },
-        {
-          path: PATH.SURVEY,
-          element: <SurveyPage />,
-        },
-        {
-          path: PATH.CUSTOM_INFO,
-          element: <CustomInfoPage />,
-        },
-        {
-          path: PATH.POLICY_LIST,
-          element: <PolicyListPage />,
-        },
-        {
-          path: PATH.POLICY_DETAIL,
-          element: <PolicyDetailPage />,
-        },
-        {
-          path: PATH.POLICY_SEARCH,
-          element: <PolicySearchPage />,
-        },
-        {
-          path: PATH.EXCEPTION,
-          element: <NotFoundPage />,
-        },
-        {
-          path: PATH.LOADING,
-          element: <LoadingPage />,
-        },
-      ],
-
-      errorElement: <div>error page</div>,
-    },
-  ],
+const CustomInfoPage = lazy(
+  () => import(/* webpackChunkName: "CustomInfoPage" */ '@/pages/CustomInfoPage'),
 );
+const Homepage = lazy(() => import(/* webpackChunkName: "Homepage" */ '@/pages/Homepage'));
+const IntroPage = lazy(() => import(/* webpackChunkName: "IntroPage" */ '@/pages/IntroPage'));
+const LoadingPage = lazy(() => import(/* webpackChunkName: "LoadingPage" */ '@/pages/LoadingPage'));
+const NotFoundPage = lazy(
+  () => import(/* webpackChunkName: "NotFoundPage" */ '@/pages/NotFoundPage'),
+);
+const PolicyDetailPage = lazy(
+  () =>
+    import(/* webpackChunkName: "PolicyDetailPage" */ '@/pages/PolicyDetailPage/PolicyDetailPage'),
+);
+const PolicyListPage = lazy(
+  () => import(/* webpackChunkName: "PolicyListPage" */ '@/pages/PolicyListPage'),
+);
+const PolicySearchPage = lazy(
+  () =>
+    import(/* webpackChunkName: "PolicySearchPage" */ '@/pages/PolicySearchPage/PolicySearchPage'),
+);
+const SurveyPage = lazy(
+  () => import(/* webpackChunkName: "SurveyPage" */ '@/pages/SurveyPage/SurveyPage'),
+);
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <IntroPage />,
+      },
+      {
+        path: PATH.HOME,
+        element: <Homepage />,
+      },
+      {
+        path: PATH.SURVEY,
+        element: <SurveyPage />,
+      },
+      {
+        path: PATH.CUSTOM_INFO,
+        element: <CustomInfoPage />,
+      },
+      {
+        path: PATH.POLICY_LIST,
+        element: <PolicyListPage />,
+      },
+      {
+        path: PATH.POLICY_DETAIL,
+        element: <PolicyDetailPage />,
+      },
+      {
+        path: PATH.POLICY_SEARCH,
+        element: <PolicySearchPage />,
+      },
+      {
+        path: PATH.EXCEPTION,
+        element: <NotFoundPage />,
+      },
+      {
+        path: PATH.LOADING,
+        element: <LoadingPage />,
+      },
+    ],
+
+    errorElement: <div>error page</div>,
+  },
+]);
