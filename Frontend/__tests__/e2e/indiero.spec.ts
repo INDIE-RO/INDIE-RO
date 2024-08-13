50810daab81a4642670092aa3e1d84a22d0d8298
hae-on <solbi2004@naver.com>
2024-08-13 23:52:53 +0900

    test: playwright 설문조사 테스트
    Co-authored-by: Hyeryong Choi <HyeryongChoi@users.noreply.github.com>

--- Frontend/__tests__/e2e/indiero.spec.ts
+++ Frontend/__tests__/e2e/indiero.spec.ts
@@ -1,11 +1,31 @@
 import { expect, test } from '@playwright/test';
 
-test('has wordCloud keyword', async ({ page }) => {
-  await page.goto('https://indiero.com/home');
+test('should display input with correct placeholder after clicking keyword', async ({ page }) => {
+  await page.goto('localhost:3000/home');
 
   await page.getByText('자립', { exact: true }).click();
 
   const inputElement = await page.locator('input[placeholder="자립"]');
 
   await expect(inputElement).toBeVisible();
+});
+
+test('should display custom information box after survey completion', async ({ page }) => {
+  await page.goto('localhost:3000');
+
+  await page.getByRole('button', { name: '설문 시작하기' }).click();
+  await page.locator('input[type="checkbox"][value="교육"]').check();
+  await page.getByRole('button', { name: '다음' }).click();
+  await page.locator('input[type="checkbox"][value="서울"]').check();
+  await page.getByRole('button', { name: '다음' }).click();
+  await page.locator('input[type="checkbox"][value="25~29세"]').check();
+  await page.getByRole('button', { name: '완료' }).click();
+
+  await page.waitForTimeout(2000);
+
+  const customInfoBox = page.locator('[data-testid="custom-info-box"]');
+
+  await expect(customInfoBox).toBeVisible();
+
+  await expect(customInfoBox).toContainText(['25~29세 • 교육 • 서울']);
 });
