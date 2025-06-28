6d9aa0d45bad45d7213d516cf19e1db698e9aed5
HyeryongChoi <chex1004@gmail.com>
2025-06-29 03:41:51 +0900

    fix: 백그라운드 이미지 public 폴더로 이동 및 경로 수정

--- Frontend/src/components/@common/Layout/DesktopLayout.tsx
+++ Frontend/src/components/@common/Layout/DesktopLayout.tsx
@@ -17,7 +17,7 @@ const DesktopLayoutStyle = css`
 
     padding-left: 55%;
 
-    background: url('./assets/indieroBackground.svg') no-repeat center;
+    background: url(${process.env.PUBLIC_URL}/assets/indieroBackground.svg);
     background-size: cover;
 
     overflow-y: auto;
